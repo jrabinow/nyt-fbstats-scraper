ae8ddd79b24c6c63281ddca4b1b8e40c91121c36
jrabinow <randprefix.github@fastmail.com>
2014-07-30 16:27:50 -0400

    Able to succesfully extract and decode all nyti.ms URLS on facebook page

--- 2test_downloader.py
+++ 2test_downloader.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-import urllib2
+import urllib, urllib2
 import re
 import HTMLParser
 
-url = "http://facebook.com/nytimes/posts/10150428621164999"
+url = "http://facebook.com/nytimes/" #posts/10150428621164999"
 
 def http_download(url):
     try:
@@ -16,27 +16,41 @@ def http_download(url):
         exit()
 
 def unescape_url(url_str):
-    return re.sub(r"\\", "", url_str)
+    return re.sub(r"\\\\/", "/", url_str)
 
-#def decode_unicode_url(url_str):
-#    return 
+def decode_unicode_url(url_str):
+    return decode_quoted_url(re.sub(r"\\\\(u[0-9A-F]{6})", r"\\\1", url_str).decode('unicode-escape'))
+
+def decode_quoted_url(url_str):
+    return urllib.unquote(url_str)
 
 def main():
     html_page = http_download(url)
     escaped_seq = re.compile(r"^https?:(\\{2}/){2}nyti\.ms\\{2}/[a-zA-Z0-9]+$")
     unicode_seq = re.compile(r"^https?\\{2}u00253A(\\{2}u00252F){2}nyti\.ms\\{2}u00252F[a-zA-Z0-9]+$")
     url_encoded_seq = re.compile(r"^https?%3A%2F%2Fnyti.ms%2F[a-zA-Z0-9]+$")
+    clean_url_seq = re.compile(r"^https?://nyti.ms/[a-zA-Z0-9]+$")
     parser = HTMLParser.HTMLParser()
 
-    for element in re.findall("https?.{,30}nyti\.ms[^&]*(?=&)", html_page):
+    url_list = {}
+
+    for element in re.findall("https?.{,30}nyti\.ms[/a-zA-Z0-9\\%]+", html_page):
         if escaped_seq.match(element):
-            print "ESCAPED: " + unescape_url(element)
+            target_url = unescape_url(element)
         elif unicode_seq.match(element):
-            print "UNICODE: " + element
+            target_url = decode_unicode_url(element)
         elif url_encoded_seq.match(element):
-            print "URL ENCODED: " + parser.unescape(element)
+            target_url = decode_quoted_url(element)
+        elif clean_url_seq.match(element):
+            target_url = element
         else:
+            print element + "$"
             raise Exception("Invalid match")
+        if target_url not in url_list:
+            url_list[target_url] = True
+
+    for target_url in url_list:
+        print target_url
 
 if __name__ == "__main__":
     main()
